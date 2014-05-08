ac892e042ab0767fbf06f1c223213dd6b5c8f2c4
unknown <dneedham@DNeedham-520.clientserver.com>
2014-05-07 22:09:34 -0400

    adding

--- Plot1.R
+++ Plot1.R
@@ -4,7 +4,7 @@ mindate <- as.Date("2007-02-01",format="%Y-%m-%d")
 maxdate <- as.Date("2007-02-02",format="%Y-%m-%d")
 full.DF <- alldata[as.Date(alldata$Date,format="%d/%m/%Y") >= mindate & as.Date(alldata$Date,format="%d/%m/%Y") <=maxdate,]
 rm(alldata)
-full.DF$Date <- as.Date(full.DF$Date,format="%m/%d/%Y")
+full.DF$Date <- as.Date(full.DF$Date,format="%d/%m/%Y")
 png("plot1.png")
 hist(full.DF$Global_active_power,col="red",xlab="Global Active Power (Kilowatts)",main="Global Active Power")
 dev.off()
