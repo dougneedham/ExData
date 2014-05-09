a2c30f38c0682cb51f2abc3c1fc736e55973576a
unknown <dneedham@DNeedham-520.clientserver.com>
2014-05-08 21:49:12 -0400

    adding plot4

---
+++ plot4.r
@@ -0,0 +1,20 @@
+filename <- "household_power_consumption.txt"
+alldata <- read.table(filename,sep=";",header=TRUE,colClasses= c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric') ,skip=0,na.strings="?")
+mindate <- as.Date("2007-02-01",format="%Y-%m-%d")
+maxdate <- as.Date("2007-02-02",format="%Y-%m-%d")
+full.DF <- alldata[as.Date(alldata$Date,format="%d/%m/%Y") >= mindate & as.Date(alldata$Date,format="%d/%m/%Y") <=maxdate,]
+rm(alldata)
+full.DF$Date <- as.Date(full.DF$Date,format="%d/%m/%Y")
+full.DF$DateTime <- strptime(paste(full.DF$Date, full.DF$Time), format="%Y-%m-%d %H:%M:%S")
+png("plot4.png")
+par(mfrow=c(2,2))
+plot(full.DF$DateTime,full.DF$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+plot(full.DF$DateTime,full.DF$Voltage,type="l",xlab="datetime",ylab="Voltage")
+plot(full.DF$DateTime,full.DF$Sub_metering_1,type="l",ylab="Global Active Power (kilowatts)",xlab="",col="blue")
+lines(full.DF$DateTime,full.DF$Sub_metering_2,col="red")
+lines(full.DF$DateTime,full.DF$Sub_metering_3,col="green")
+legend("topright", c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"), 
+       lty=c(1,1,1),
+       lwd=c(2.5,2.5,2.5),col=c("blue","red","green"))
+plot(full.DF$DateTime,full.DF$Global_reactive_power,type="l",xlab="datetime",ylab="Global_Reactive_power")
+dev.off()
